b038efc2bd9a31c3d0a86e74d7e1d248719dfc74
suraj singh rajput <surajthakurrs45@gmail.com>
2024-10-01 12:41:08 +0530

    server

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ io.on("connection", (socket) => {
 });
 
 // Use the port assigned by Render
-const PORT = process.env.PORT || 3001;
+const PORT =  process.env.PORT || 3001;;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
